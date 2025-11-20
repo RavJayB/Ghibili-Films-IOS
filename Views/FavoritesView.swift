280ae32cd7c46ad374ca7337ba1ca529091ec82a
RavJayB <ravindujayb@gmail.com>
2025-11-21 02:35:44 +0530

    Add images for favorites

--- Views/FavoritesView.swift
+++ Views/FavoritesView.swift
@@ -32,12 +32,34 @@ struct FavoritesView: View {
                         NavigationLink {
                             FilmDetails(film: film)
                         } label: {
+                            
                             VStack(alignment: .leading) {
+                                
                                 Text(film.title)
                                     .font(.headline)
                                 Text(film.director)
                                     .foregroundStyle(.secondary)
                             }
+                            
+                            HStack(){
+                                               
+                                if let url = URL(string: film.image) {
+                                                    AsyncImage(url: url) { phase in
+                                                        switch phase {
+                                                        case .success(let image):
+                                                            image.resizable().scaledToFit()
+                                                        case .failure(_):
+                                                            Image(systemName: "book.closed")
+                                                        case .empty:
+                                                            ProgressView()
+                                                        @unknown default:
+                                                            EmptyView()
+                                                        }
+                                                    }
+                                                    .frame(height: 200)
+                                                }
+                            }
+                            .frame(maxWidth: .infinity, alignment: .trailing)
                         }
                     }
                     .onDelete(perform: viewModel.removeFavorite)
